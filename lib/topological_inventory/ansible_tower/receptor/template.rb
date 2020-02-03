fca6fd74a63e42683989a18aaf172d0ae607001d
Martin Slemr <mslemr@redhat.com>
2020-02-03 13:37:49 +0100

    Launch Job Template

--- lib/topological_inventory/ansible_tower/receptor/template.rb
+++ lib/topological_inventory/ansible_tower/receptor/template.rb
@@ -2,7 +2,14 @@ module TopologicalInventory::AnsibleTower
   module Receptor
     class Template < ApiObject
       def launch(post_data)
+        path = File.join(endpoint, id.to_s, '/')
 
+        response = send_request(:post, path, post_data)
+        parse_response(response)
+
+        job = JSON.parse(response)
+
+        api.jobs.find(job['job'])
       end
     end
   end
